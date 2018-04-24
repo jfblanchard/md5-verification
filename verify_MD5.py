36a4ee71d92c66eee4eea180ed75d867121bbeb1
jfblanchard <jonflintblanchard@gmail.com>
2018-04-24 08:04:00 -0400

    Converting to use python hashlib instead of certUtil.  Work in progress. Adding .ui file for adding a GUI

--- verify_MD5.py
+++ verify_MD5.py
@@ -2,14 +2,17 @@
 """
 Functions for performing a MD5 hash sum verification.
 
-Calls the windows program certUtil.
-Nexst version will run from the python language for platform independance.
+#Calls the windows program certUtil.
+Updating to use the python moduld hashlib for platform independence.
+Todo:  Support more hashes besides md5
+make a drop down menu of hash types
 
 Warn the user that MD5 has been broken.
 
 """
 
-import subprocess, sys
+import sys
+import hashlib
 from tkinter import Tk
 from tkinter import filedialog
 
@@ -31,34 +34,55 @@ def get_file(myTitle=None):
 
                     
 
-def verify_MD5(hash_sum, filename):
+def verify_MD5(hash_string, filename):
+    """ Function to compute hash sums.
     
-
-
-    out = subprocess.check_output('certUtil -hashfile '+filename +' MD5')
-    start = out.find(b'\n') + 1 #starts right after the first '\n'
-    end = out.find(b'CertUtil')-2
-                  
-    md5_string = str('')
-    for i in range(start, end, 2):
-        md5_string = md5_string + out[i:i+2].decode('utf-8')
+    """
     
-    md5_string = md5_string[0:-1]           #remove the \r at end
-    md5_string = md5_string.replace(' ','')   #remove white spaces...make optional?
+    hashes = hashlib.algorithms_available
+    
+    #h = hashlib.new('md5')
+    h = hashlib.new('sha256')
+    
+    with open(filename,'rb') as f:
+        for block in iter(lambda: f.read(128), b""):
+            h.update(block)
+        
+    computed_sum = h.hexdigest()
 
-    print ('MD5 Hash Sum: ' + md5_string)
-    print ('Computed: ' + md5_string)
-    if md5_string == hash_sum:
+    print ('MD5 Hash Sum: ' + hash_string)
+    print ('Computed: ' + computed_sum)
+    
+    print_results(hash_string,computed_sum)
+    
+def print_results(hash_string,computed_sum):
+    """Print the results of the calculation
+    """
+    
+    if hash_string == computed_sum:
         print ('SUCCESS! Sums match')
     else:
         print('FAIL!. Sums do not match')
 
+
+def test_hash_sum():
+    """Test function currently using the one from the python docs."""
+    
+    test_string = 'Nobody knows the trouble Ive seen'
+
+    m = hashlib.new('md5')
+    m.update(test_string.encode('utf-8'))
+    computed = m.hexdigest()
+    
+    answer = '2e7bb7e7d27b8cda5492e24345dd1c69'
+    kali = 'ed88466834ceeba65f426235ec191fb3580f71d50364ac5131daec1bf976b317'
+    print_results(computed,answer)
     
 
 if __name__ == '__main__':
     
     hash_sum = sys.argv[1]
-    fname = get_file('Please choose a file for MD5 checksum')
+    fname = get_file('Please choose a file for checksum')
     verify_MD5(hash_sum, fname)
     
 
